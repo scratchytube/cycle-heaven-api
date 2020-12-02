8621d3004c9a516657bb01c951f84e963e34a8e7
techtwins <gshevaughn@gmail.com>
2020-12-01 21:16:43 -0500

    added actions for the controllers

--- app/controllers/api/v1/modification_requests_controller.rb
+++ app/controllers/api/v1/modification_requests_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::ModificationRequestsController < ApplicationController
+  
+  def index
+    appointments = ModificationRequest.all
+    render json: appointments
+  end
+
+  def show
+    appointment = ModificationRequest.find(params[:id])
+    render json: appointment
+  end
+
+  def create
+    appointment = ModificationRequest.create!(appointment_params)
+    render json: appointment
+  end
+
+  def destroy
+    appointment = ModificationRequest.find(params[:id])
+    appointment.destroy! 
+    render json: {}
+  end
+
+  def update
+    appointment = ModificationRequest.find(params[:id])
+    appointment.update!(appointment_params)
+    render json: appointment
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:time, :day)
+  end
 end
--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::MotorcyclesController < ApplicationController
         render json: motorcycles
     end
 
+    def show
+      motorcycle = Motorcycle.find(params[:id])
+      
+      render json: motorcycle
+    end
+
+    private
+
+    def motorcycle_params
+      params.permit(:maker, :name, :year, :imageUrl)
+    end
 end
